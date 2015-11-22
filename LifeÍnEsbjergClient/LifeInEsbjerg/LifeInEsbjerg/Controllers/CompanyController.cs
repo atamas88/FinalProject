12c6686d2d05998f11d455d11065977d918e4b16
kriahja <kriahja@gmail.com>
2015-11-22 14:13:15 +0100

    Client: CompanyController Added (ReadAll() with small View for testing connection) Client: ViewModel added for later use

---
+++ LifeÍnEsbjergClient/LifeInEsbjerg/LifeInEsbjerg/Controllers/CompanyController.cs
@@ -0,0 +1,24 @@
+﻿using DtoModel;
+using LifeInEsbjergGateway;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LifeInEsbjerg.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private Facade facade = new Facade();
+
+        // GET: Company
+        public ActionResult Index()
+        {
+            IEnumerable<Company> companies = facade.GetComapnyGateway().ReadAll();
+            Debug.WriteLine(companies);
+            return View(companies);
+        }
+    }
+}
--- LifeÍnEsbjergClient/LifeInEsbjerg/LifeInEsbjergGateway/Facade.cs
+++ LifeÍnEsbjergClient/LifeInEsbjerg/LifeInEsbjergGateway/Facade.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LifeInEsbjergGateway.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace LifeInEsbjergGateway
 {
-    class Facade
+   public class Facade
     {
+        public CompanyGatewayService GetComapnyGateway()
+        {
+            return new CompanyGatewayService();
+        }
     }
 }
